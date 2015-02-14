934155a0f01043268e095cf737598dcfb5c7cce1
pumuckl007 <pumuckl007>
2015-02-13 20:22:57 -0800

    Added Unit Tests

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c5/10aaab5749b300141dfc8502a16dc146
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team2984.robot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MecanumDriveTest {
+	
+	@Test
+	public void driveWithFowardSpeedSetToOneMovesFowardAtSpeedOne() {
+		fail("Not yet implemented");
+	}
+
+}
--- Mecanum Drive/src/org/usfirst/frc/team2984/robot/Robot.java
+++ Mecanum Drive/src/org/usfirst/frc/team2984/robot/Robot.java
@@ -1,25 +1,19 @@
 package org.usfirst.frc.team2984.robot;
 
 
-import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.RobotDrive.MotorType;
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * This is a demo program showing how to use Mecanum control with the RobotDrive class.
  */
 public class Robot extends IterativeRobot {
 	
-    MecanumDrive mecanumDrive;
-    RobotDrive drive;
-    boolean unSafeToggle = false;
-    boolean unSafeMode = true;
-    Joystick stick;
+    private MecanumDrive mecanumDrive;
+    private boolean unSafeToggle = false;
+    private boolean unSafeMode = true;
+    private Joystick stick;
     
     // The channel on the driver station that the joystick is connected to
     final int joystickChannel	= 0;
@@ -27,7 +21,6 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
         stick = new Joystick(joystickChannel);
         mecanumDrive = new MecanumDrive(3,4,5,1);
-//        drive = new RobotDrive(new CANTalon(3), new CANTalon(5), new CANTalon(4), new CANTalon(1));
     }
         
 
@@ -35,31 +28,23 @@ public class Robot extends IterativeRobot {
      * Runs the motors with Mecanum drive.
      */
     public void teleopPeriodic() {
-//        if(Math.abs(stick.getX())>-0.4 || Math.abs(stick.getY())>-0.4){
-//        	if(leftButton){
-//            	drive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), 0.4, 0);
-//        	} else if(rightButton) {
-//            	drive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), -0.4, 0);
-//        	} else {
-//            	drive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), 0, 0);
-//        	}
-//        }
-        	mecanumDrive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), stick.getTwist(), 0);
-        	mecanumDrive.update();
-        	if(stick.getRawButton(4)){
-        		if(!this.unSafeToggle){
-        			this.unSafeToggle = true;
-        			this.unSafeMode = !this.unSafeMode;
-        			if(this.unSafeMode){
-        				this.mecanumDrive.setVoltageRamp(2.5);
-        			} else {
-        				this.mecanumDrive.setVoltageRamp(-1);
-        			}
-        		}
-        	} else {
-        		this.unSafeToggle = false;
-        	}
-        	
+    	long time = System.currentTimeMillis();
+    	this.mecanumDrive.drive(stick.getX(), -stick.getY(), stick.getTwist(), 0);
+    	this.mecanumDrive.update();
+    	if(stick.getRawButton(4)){
+    		if(!this.unSafeToggle){
+    			this.unSafeToggle = true;
+    			this.unSafeMode = !this.unSafeMode;
+    			if(this.unSafeMode){
+    				this.mecanumDrive.setSafeMode(true, 4, 40);
+    			} else {
+    				this.mecanumDrive.setSafeMode(false, -1, -1);
+    			}
+    		}
+    	} else {
+    		this.unSafeToggle = false;
+    	}
+        SmartDashboard.putNumber("Length Of Cycle", System.currentTimeMillis()-time);	
     }
     
 }
